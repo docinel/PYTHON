aa17c49b7a21c07daaad1cd95f7b7716b7b7c2b4
docinel <docinel@gmail.com>
2023-11-02 22:33:03 -0300

    Desafios DIO

---
+++ source/Sis_Bank/sisbank.py
@@ -0,0 +1,49 @@
+menu = """
+[0] Depositar
+[1] Sacar
+[2] Extrato
+[3] Sair
+"""
+print(menu)
+
+saldo = 0
+limite = 500
+extrato = ""
+numero_saques = 0
+LIMITE_SAQUES = 3
+
+while True:
+    opcao = input("menu: ")
+    if opcao == "0":
+        valor = float(input("Quanto deseja depositar? "))
+        if valor > 0:
+            saldo += valor
+            extrato += f"Depósito: R$ {valor:.2f}\n"
+        else:
+            print("Operação falhou! O valor informado é inválido.")
+    elif opcao == "1":
+        valor = float(input("Quanto deseja sacar? "))
+        excedeu_saldo = valor > saldo
+        excedeu_limite = valor > limite
+        excedeu_saques = numero_saques >= LIMITE_SAQUES
+        if excedeu_saldo:
+            print("Operação falhou! Você não tem saldo suficiente.")
+        elif excedeu_limite:
+            print("Operação falhou! O valor do saque excede o limite.")
+        elif excedeu_saques:
+            print("Operação falhou! Número de saques excedido.")
+        elif valor > 0:
+            saldo -= valor
+            extrato += f"Saque: R$ {valor:.2f}\n"
+            numero_saques += 1
+        else:
+            print("Operação falhou! O valor informado é inválido.")
+    elif opcao == "2":
+        print("\n========================= EXTRATO ==========================")
+        print("Não foram realizadas movimentações." if not extrato else extrato)
+        print(f"\nSaldo: R$ {saldo:.2f}")
+        print("==============================================================")
+    elif opcao == "3":
+        break
+    else:
+        print("Operação inválida, por favor selecione novamente a operação desejada.")
---
+++ source/aula_01.py
@@ -0,0 +1,4 @@
+print('Rodrigo')
+
+nome = input('Qual é o seu nome? ')
+print('É um prazer te conhecer, {}!'.format(nome))
